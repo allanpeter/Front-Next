14731924781e5ce954dd276d932061063e40dc2c
mateusLinux <mateusfma2bit@gmail.com>
2023-12-23 11:29:35 -0300

    renomear pastas

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -95,6 +95,10 @@ const Sidebar = () => {
           </li>
         ))}
       </ul>
+      <button className={styles.logout}>
+        <MdLogout/>
+        Logout
+      </button>
     </div>
   );
 };
