31811dee69f097648082e9d1074c5f41dfa9a297
MATEUS FELIPE ('JAPÃO') <129597459+Japazin@users.noreply.github.com>
2024-01-03 09:30:59 -0300

    ok

---
+++ app/ui/dashboard/pagination/pagination.jsx
@@ -0,0 +1,11 @@
+import Styles from "./pagination.module.css"
+
+const Pagination = () => {
+return(
+    <div className={Styles.container}>
+        <button className={Styles.button}>Previus</button>
+        <button className={Styles.button}>Previus</button>
+    </div>
+)
+}
+export default Pagination
